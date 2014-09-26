b402d3e0d485ddc53a0503e091b955b1de49fd9b
danarch <dylan.massell@gmail.com>
2014-09-26 11:21:05 -0400

    wrote spec around remembering customer payment info

--- app/services/card_processor.rb
+++ app/services/card_processor.rb
@@ -8,7 +8,7 @@ class CardProcessor
   def process
     begin
       @customer = get_Stripe_customer @invoice.shopper
-      ## apply_coupon 
+      ## apply_coupon
       process_charge! @customer
     rescue Stripe::CardError => e
       raise ProcessingError, e.message
@@ -46,5 +46,22 @@ class CardProcessor
   def send_receipt
     ## InvoiceMailler.receipt(@invoice).deliver
     MailReceiptWorker.perform_async @invoice.id
+  end
+
+  def self.token_for_card number
+    token = Stripe::Token.create(card: {
+      number: number,
+      exp_month: 1,
+      exp_year:  2015,
+      cvc:       '777'
+    })
+    token.id
+  end
+  def self.valid_card
+    token_for_card '4242424242424242'
+  end
+
+  def self.declined_card
+    token_for_card '4000000000000002'
   end
 end
