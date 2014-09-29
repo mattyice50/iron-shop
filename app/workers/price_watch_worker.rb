659326f8303b51e8df57d04390d3caedcc2fb68e
Khin Latt <k.maunglatt@gmail.com>
2014-09-29 09:54:28 -0400

    Comment logic on perform action

--- app/workers/price_watch_worker.rb
+++ app/workers/price_watch_worker.rb
@@ -6,11 +6,20 @@ class PriceWatchWorker
     hourly
   end
 
-  def perform(last_occurence, current_occurence)
+  def perform  # (last_occurence, current_occurence)
     PriceWatch.all.each do |pricewatch|
-      if #pricewatch's item for current_occurence != last_occurence ,call PriceWatchMailer
-        # needs to do more here
-      end
+      # The rest, but this method logic, should work for background workers and checking hourly
+      
+      # if #pricewatch's item for current_occurence != last_occurence ,call PriceWatchMailer
+      #   # needs to do more here
+      #   # or without using last_occurence, current_occurence, put another column on price_watches
+      #   # and have a boolean to check whether email is sent or not
+      #   # Perhaps, have another column called recent prices when a customer clicked and then
+      #   # here compare item.price and item.last_price here
+      #   if # price changes
+      #     PriceWatchMailer.price_alert(item).deliver
+      #   end
+      # end
     end
   end
 end
